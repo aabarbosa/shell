93957d80e95cb84c6891a2dbf75abc20abd6ca7a
aabarbosa <aabarbosa.cs@gmail.com>
2018-10-03 10:05:47 -0300

    progress

--- fast_copy.sh
+++ fast_copy.sh
@@ -1,13 +1,22 @@
+# preprocess files removing spaces
+find $1 -name "* *.epub" -type f -print0 | \
+while read -d $'\0' f; 
+do 
+  	mv "$f" "${f// /_}"; 
+done
+
+# basename, dirname, they do not behave properly when filename contain space.
 DIR_PATH=`dirname $1`/shadow
 REP_NUMBER=0
 mkdir $DIR_PATH
-for j in `find $1 -name \*.java`
+for j in `find $1 -name \*.epub`
 do 
 	BASENAME=`basename $j`
 	if [ -e $DIR_PATH/$BASENAME ]; then
 		cp $j $DIR_PATH
 	else 
-		cp $j $DIR_PATH.REP_NUMBER
-		REP_NUMBER 
+		mkdir "./shadow/$REP_NUMBER"
+		cp $j $DIR_PATH/REP_NUMBER
+		$REP_NUMBER++
 	fi
 done
