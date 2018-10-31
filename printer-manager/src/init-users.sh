92f12efd0dfd652313ea85f1ef84cc808655d896
aabarbosa <alekssandro.barbosa@ccc.ufcg.edu.br>
2018-10-31 17:54:00 -0300

    not

---
+++ printer-manager/src/init-users.sh
@@ -0,0 +1,9 @@
+#!bash/bin
+
+# Initialize users
+ls /home >> ./users
+
+# At the end of each line related to each user add the monthly cote.
+# cote: a cote is the number of <pages> a user can print.
+# <pages> contain 3600 characters
+awk '{}'
