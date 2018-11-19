4299f5407039b58a8359d19b88e11ffc23757d1f
aabarbosa <aabarbosa.cs@gmail.com>
2018-11-19 13:07:31 -0300

    Logger using time

--- logging/init.sh
+++ logging/init.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
 while :
-    do
-        read value
-        $value
+   	do
+        read program
+        #if [ $program = command ]; then 
+        #    echo "ignore"
+        #else 
+        exec="$(time ( $program ) 2>&1 1>/dev/null )"
+        #fi        
+        # Write in the log file <output.log>
+        echo $exec >> output.log 
 
     done
  
